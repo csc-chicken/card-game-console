5d4b922247cab0fd9237f3fff687e4a618bd96f5
unknown <leo.yang@cimforce.com>
2021-05-18 13:24:50 +0800

    加入專案檔案。

---
+++ card game console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace card_game_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cardgame Mycardgame = new Cardgame();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
